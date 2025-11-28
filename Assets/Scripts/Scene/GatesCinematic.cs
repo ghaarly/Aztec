a1f2cb8646780e5d6350ec4e98ddb54b0c2cce46
Gharly <fedegvivas@gmail.com>
2025-11-27 22:07:05 -0300

    AAAAAAAAAAAAAA
    Palmeras. Bushes. Cinematica.

---
+++ Assets/Scripts/Scene/GatesCinematic.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GatesCinematic : MonoBehaviour
+{
+    [Header("Enemigos / Objetos a eliminar")]
+    public List<GameObject> targets;
+    [Header("Jugador")]
+    public GameObject player;
+    public MonoBehaviour[] playerControlScripts;
+    public Canvas gameCanvas;
+
+    [Header("Objetos")]
+    public GameObject Door;
+    public GameObject Deteails;
+    public GameObject objectToActivate;      
+    [Header("Cámaras")]
+    public Camera gameCamera;
+    public Camera cinematicCamera;
+    [Header("Fade")]
+    public Image fadeImage;
+    public float fadeTime = 1f;
+
+    [Header("Animación final")]
+    public Animator gateAnim;
+    public string animationTrigger = "OpenGate";
+    public float secondsToWait = 3f;
+    [Header("Siguiente escena")]
+    public string nextSceneName;
+
+    private bool sequenceStarted = false;
+
+    void Start()
+    {
+        if (cinematicCamera != null)
+            cinematicCamera.gameObject.SetActive(false);
+
+        fadeImage.color = new Color(0, 0, 0, 0);
+
+        StartCoroutine(CheckTargetsRoutine());
+    }
+
+    IEnumerator CheckTargetsRoutine()
+    {
+        while (!sequenceStarted)
+        {
+            bool allDestroyed = true;
+
+            foreach (var t in targets)
+            {
+                if (t != null)
+                {
+                    allDestroyed = false;
+                    break;
+                }
+            }
+
+            if (allDestroyed)
+            {
+                sequenceStarted = true;
+                StartCoroutine(EndSequence());
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    IEnumerator EndSequence()
+    {
+        foreach (var script in playerControlScripts)
+            script.enabled = false;
+        if (gameCanvas != null)
+            gameCanvas.enabled = false;
+
+        yield return StartCoroutine(FadeToBlack());
+        gameCamera.gameObject.SetActive(false);
+        cinematicCamera.gameObject.SetActive(true);
+        yield return StartCoroutine(FadeFromBlack());
+
+        if (Door != null)
+            Door.SetActive(false);
+
+        if (Deteails != null)
+            Deteails.SetActive(false);
+        if (objectToActivate != null)
+            objectToActivate.SetActive(true);
+
+        gateAnim.SetTrigger(animationTrigger);
+        yield return new WaitForSeconds(secondsToWait);
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        float t = 0;
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, t / fadeTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeFromBlack()
+    {
+        float t = 0;
+        while (t < fadeTime)
+        {
+            t += Time.deltaTime;
+            fadeImage.color = new Color(0, 0, 0, 1 - (t / fadeTime));
+            yield return null;
+        }
+    }
+}
